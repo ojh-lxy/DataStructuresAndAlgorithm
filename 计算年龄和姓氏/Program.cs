f7eb46d6bf46e3b16d2c0e7faa0c7ba7394b5565
ojh-lxy <244973981@qq.com>
2019-12-17 09:53:29 +0800

    计算年龄

---
+++ 计算年龄和姓氏/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using 计算年龄和姓氏.Properties;
+
+namespace 计算年龄和姓氏
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {            
+            var result = "";
+            var totalDays = 0;  
+            var now = DateTime.Now;
+            ListClass list = new ListClass();
+            foreach (var item in list.InitialPersonList())
+            {
+                totalDays = totalDays + Math.Abs(((TimeSpan)(now - item.Birthday)).Days);
+            }
+
+            var avgTotaldays = totalDays / list.InitialPersonList().Count;
+            var avgYears = avgTotaldays / 365;
+            var avgDays = avgTotaldays - avgYears * 365;
+
+            result = "平均年龄：" + avgYears + "周岁" + "-" + avgDays + "天";
+            Console.Write(result);            
+        }
+    }
+}
---
+++ 计算年龄和姓氏/Properties/Person.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace 计算年龄和姓氏.Properties
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public string City { get; set; }
+        public bool Sex { get; set; }
+        public DateTime Birthday { get; set; }
+        public string Email { get; set; }
+        public string Province { get; set; }
+    }
+}
