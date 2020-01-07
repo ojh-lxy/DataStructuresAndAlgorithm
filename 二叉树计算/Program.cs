ac060456158cfa245aba40b091102d594c522b80
ojh-lxy <244973981@qq.com>
2020-01-07 10:39:38 +0800

    二叉树计算层数

---
+++ 二叉树计算/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace 二叉树计算
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int ceng = 0;
+            double qian = 0;
+            for (int i = 1; qian <= 10000; i++)
+            {
+                qian += ((Math.Pow(2, i)) * 10000) * (Math.Pow(0.15, i));
+                ceng = i;
+            }
+            Console.WriteLine("发展层数："+ceng);
+        }
+    }
+}
